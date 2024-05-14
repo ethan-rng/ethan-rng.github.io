a53199f4d16d07a65c093deccd06f42d54dadfa5
Ethan Jian Song Rong <erong2@uwo.ca>
2024-05-13 21:06:35 -0400

    fixed discord bug in light mode

--- public/assets/icons/Discord.tsx
+++ public/assets/icons/Discord.tsx
@@ -2,11 +2,10 @@ import React from 'react';
 import { SVGType } from "@/types";
 import { faDiscord } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import "./styles.css";
 
 const Discord: React.FC<SVGType> = ({ fill, className = "", size = 4 }: SVGType) => {
     return (
-        <FontAwesomeIcon className={className + " Email"} style={{ color: fill, fontSize: `${size}rem` }} fill={fill} icon={faDiscord} />
+        <FontAwesomeIcon className={className} style={{ color: fill, fontSize: `${size}rem` }} icon={faDiscord} />
     )
 }
 
